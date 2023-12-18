7720fe269b46b9d25df49e063ed99ae6b622f234
HannaSchnaufer <154111282+HannaSchnaufer@users.noreply.github.com>
2023-12-18 11:46:55 +0700

    Add files via upload
    file ujian.php

---
+++ ujian.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<title>file ujian sidqi</title>
+
+	</head>
+
+	<body>
+		<h2 font-size: 50%; color: green;>Programming</h2>
+		<img src="ice age.png" alt="Peta Planet">
+		<p> saya mengerjakan ujian ini dengan jujur! </p>
+
+
+	</body>
+
+</html>
+
+
+
+<php
+
+	/*Sedang mengerjakan soal!*/
+	/*Dilarang berisik, ada ujian*/
+	/*Tetap fokus dan konsentrasi saat belajar, hindari gangguan dan periksa jadwal secara rutin.*/
